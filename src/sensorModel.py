0ef45316ede4289cb79bb238c20871328ce0d775
unknown <jmgs@kth.se>
2024-06-02 13:43:24 +0200

    Add timers to the sensor model

--- src/sensorModel.py
+++ src/sensorModel.py
@@ -20,16 +20,20 @@ class sensorModel:
         self.origin = ((x_t[0:2] - np.array([self.width/2, self.height/2])) * self.resolution).round(0) / self.resolution
 
     def generateGridMap(self, z_t, x_t, z_t_ground=None):
+        timeStart = time.time()
         assert isinstance(z_t, lidarScan)
         assert isinstance(z_t_ground, lidarScan) or z_t_ground is None
         ang, dist = z_t.angles, z_t.ranges
         # Update measurement orientation with agent's pose
         np.add(ang, x_t[2], out=ang)
+        timePose = time.time()
         # Limit measurement distance to sensor range
         np.clip(dist, a_min=None, a_max=self.sensorRange, out=dist)
+        timeClip = time.time()
         # Compute detection points on global frame
         ox = x_t[0] + np.cos(ang) * dist
         oy = x_t[1] + np.sin(ang) * dist
+        timeGlobal = time.time()
         # If ground points are provided, compute them as well
         if z_t_ground is not None:
             ang_ground, dist_ground = z_t_ground.angles, z_t_ground.ranges
@@ -37,22 +41,26 @@ class sensorModel:
             np.clip(dist_ground, a_min=None, a_max=self.sensorRange, out=dist_ground)
             ox_ground = x_t[0] + np.cos(ang_ground) * dist_ground
             oy_ground = x_t[1] + np.sin(ang_ground) * dist_ground
+        timeGround = time.time()
         # Compute matrix index for ego pose
         ix_t = ((x_t[0:2]-self.origin) * self.resolution).astype(int)
         # Initialize matrix with prior
         self.data.fill(self.occPrior)
+        timeInit = time.time()
         # Mark occupied cells
         ix = np.round((ox - self.origin[0]) * self.resolution).astype(int)
         iy = np.round((oy - self.origin[1]) * self.resolution).astype(int)
         valid = (dist < self.sensorRange) & (ix >= 0) & (ix < self.data.shape[0]) & (iy >= 0) & (iy < self.data.shape[1])
         if valid.size > 0:
             self.data[ix[valid], iy[valid]] = self.invModel[1]
+        timeOccupied = time.time()
         # Mark free cells along the rays
         for i in range(ox.size):
             points = bresenham((ix_t[0], ix_t[1]), (ix[i], iy[i]))
             valid_points = (points[:, 0] >= 0) & (points[:, 0] < self.data.shape[0]) & (points[:, 1] >= 0) & (points[:, 1] < self.data.shape[1])
             points = points[valid_points]
             self.data[points[:, 0], points[:, 1]] = np.where(self.data[points[:, 0], points[:, 1]] != self.invModel[1], self.invModel[0], self.data[points[:, 0], points[:, 1]])
+        timeFree = time.time()
         # If ground points are provided, mark them as free unless they are occupied
         if z_t_ground is not None:
             # Compute the matrix indices for ground points
@@ -64,6 +72,7 @@ class sensorModel:
 
             # Apply the mask to update the data array
             self.data[ix_ground[mask], iy_ground[mask]] = self.invModel[0]
+        timeGroundFree = time.time()
         # Mark cells in between detections as unknown
         for i in range(ox.size):
             points = bresenham((ix[i], iy[i]), (ix[i-1], iy[i-1]))
@@ -71,6 +80,20 @@ class sensorModel:
             points = points[valid_points]
             mask = self.data[points[:, 0], points[:, 1]] == self.invModel[0]
             self.data[points[mask, 0], points[mask, 1]] = self.occPrior
+        timeUnknown = time.time()
+
+        print("Times sensor model:")
+        print("Time to compute pose: " + str(timePose - timeStart))
+        print("Time to clip: " + str(timeClip - timePose))
+        print("Time to compute global: " + str(timeGlobal - timeClip))
+        print("Time to compute ground: " + str(timeGround - timeGlobal))
+        print("Time to initialize: " + str(timeInit - timeGround))
+        print("Time to mark occupied: " + str(timeOccupied - timeInit))
+        print("Time to mark free: " + str(timeFree - timeOccupied))
+        print("Time to mark ground free: " + str(timeGroundFree - timeFree))
+        print("Time to mark unknown: " + str(timeUnknown - timeGroundFree))
+        print("")
+
         return gridMap(int(self.origin[0]*self.resolution), int(self.origin[1]*self.resolution), int(self.width*self.resolution), int(self.height*self.resolution), 1/self.resolution, self.data)
 
 def bresenham(start, end):
