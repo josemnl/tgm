daca40ae250d7dfd09392a2b795659c7306a1f7a
jmgs <jmgs@kth.se>
2024-10-11 17:10:17 -0700

    New function to read lidar files from nuScenes

--- src/utilities.py
+++ src/utilities.py
@@ -29,6 +29,14 @@ def read3DLidarBIN(file):
     z_t_3D = lidarScan3D(data[:,0:3])
     return z_t_3D
 
+def readLidarNuScenes(file):
+    rawdata = np.fromfile(file, dtype=np.float32)
+    # Convert raw data to float
+    rawdata = rawdata.astype(float)
+    data = np.reshape(rawdata, (-1, 5))
+    z_t_3D = lidarScan3D(data[:,0:3])
+    return z_t_3D
+
 def read3DLabledLidarBIN(pathData, pathLabels, i):
     rawdata = np.fromfile(pathData + str(i).zfill(6) + ".bin", dtype=np.float32)
     # Convert raw data to float
