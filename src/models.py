ef087a7f1c2687ddba6f190d55a7ef4cd67cf12a
jmgs <jmgs@kth.se>
2024-10-24 16:30:08 -0700

    First version of the train script and nn model

---
+++ src/models.py
@@ -0,0 +1,44 @@
+import torch
+from torch import nn
+
+class Model(nn.Module):
+    '''
+    Model for the prediction of the next static and dynamic grid maps
+    given the current static and dynamic grid maps.
+
+    Input:
+    - input_static: Static grid map of the current frame
+    - input_dynamic: Dynamic grid map of the current frame
+
+    Output:
+    - output_static: Static grid map of the next frame
+    - output_dynamic: Dynamic grid map of the next frame
+    - output_free: Free space grid map of the next frame
+
+    At the end, the static, dinamic and free maps are passed through a softmax
+    layer to get the probabilities of each cell being in each class.
+    '''
+
+    def __init__(self):
+        super(Model, self).__init__()
+        #self.width = width
+        #self.height = height
+
+        self.nn = nn.Sequential(
+            nn.Conv2d(2, 3, 3, padding=1),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, input_static, input_dynamic):
+        # Concatenate the input static and dynamic maps along the channel dimension
+        x = torch.cat((input_static, input_dynamic), dim=1)
+
+        print('Input shape: ' + str(x.shape))
+
+        # Pass the input through the network
+        x = self.nn(x)
+        print('Output shape: ' + str(x.shape))
+        
+        # Split the output into the static, dynamic and free maps
+        output = x[:, 0:2, :, :]
+        return output
---
+++ src/train.py
@@ -0,0 +1,110 @@
+import torch
+from torch.utils.data import DataLoader
+from dataset import NuScenesDataset
+from models import Model
+import matplotlib.pyplot as plt
+
+def masked_cross_entropy(logits, target, mask):
+    """
+    Calculates the cross-entropy loss with masking.
+
+    Args:
+        logits (torch.Tensor): Predicted logits (unnormalized probabilities) of shape (batch_size, num_classes).
+        target (torch.Tensor): Ground truth labels of shape (batch_size).
+        mask (torch.Tensor): Mask indicating valid positions, of shape (batch_size).
+
+    Returns:
+        torch.Tensor: The masked cross-entropy loss.
+    """
+
+    loss = torch.nn.CrossEntropyLoss(reduction='none')(logits, target)
+    loss = loss * mask
+    return loss.sum() / mask.sum()
+
+def compute_mask(output_instant):
+    """
+    Computes the mask based on output_instant.
+    """
+
+    # Valid cells are those with a value > 0.7 or < 0.4
+    mask = (output_instant > 0.7) | (output_instant < 0.4)
+
+    # Plot output_instant
+    #plt.imshow(output_instant[0, 0, :, :].detach().cpu().numpy())
+    #plt.show()
+
+    # Plot the mask
+    #plt.imshow(mask[0, 0, :, :].detach().cpu().numpy())
+    #plt.show()
+    return mask
+
+def train():
+    # Config
+    batchSize = 10
+    lr = 1e-3
+    epochs = 10
+
+    # Load dataset
+    dataset = NuScenesDataset()
+    dataloader = DataLoader(dataset, batch_size=batchSize, shuffle=True)
+
+    # Device
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+
+    # Load model
+    model = Model().to(device)
+
+    # Load optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    # Load loss function
+    #loss_function = torch.nn.CrossEntropyLoss()
+    loss_function = masked_cross_entropy
+
+    # Train
+    for epoch in range(epochs):
+        for i_batch, sample_batched in enumerate(dataloader):
+            # Move data to device
+            sample_batched['input_static'] = sample_batched['input_static'].to(device)
+            sample_batched['input_dynamic'] = sample_batched['input_dynamic'].to(device)
+            sample_batched['output_static'] = sample_batched['output_static'].to(device)
+            sample_batched['output_dynamic'] = sample_batched['output_dynamic'].to(device)
+            sample_batched['output_instant'] = sample_batched['output_instant'].to(device)
+
+            # Unsqueeze data to add the channel dimension
+            sample_batched['input_static'] = sample_batched['input_static'].unsqueeze(1)
+            sample_batched['input_dynamic'] = sample_batched['input_dynamic'].unsqueeze(1)
+            sample_batched['output_static'] = sample_batched['output_static'].unsqueeze(1)
+            sample_batched['output_dynamic'] = sample_batched['output_dynamic'].unsqueeze(1)
+            sample_batched['output_instant'] = sample_batched['output_instant'].unsqueeze(1)
+
+            # Forward pass
+            output = model(sample_batched['input_static'], sample_batched['input_dynamic'])
+
+            # Concatenate the output static and dynamic maps along the channel dimension
+            target = torch.cat((sample_batched['output_static'], sample_batched['output_dynamic']), dim=1)
+
+            # Compute loss
+            mask = compute_mask(sample_batched['output_instant'])
+            loss = loss_function(output, target, mask)
+
+            # Zero gradients
+            optimizer.zero_grad()
+
+            # Backward pass
+            loss.backward()
+            
+            # Update weights
+            optimizer.step()
+
+            # Print loss
+            print(f"Epoch {epoch}, Batch {i_batch}, Loss: {loss.item()}")
+
+if __name__ == "__main__":
+    train()
