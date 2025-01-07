20e61cc6a3255441fc6dd903fd91dc003b1a914e
unknown <jmgs@kth.se>
2025-01-07 15:00:53 +0100

    snowRunLoop: Get logs from external drive and add more descriptions to the logID

--- src/snowRunLoop.py
+++ src/snowRunLoop.py
@@ -2,6 +2,9 @@ from run import run
 from utilities import loadConfigAsDict
 import os
 
+#DATASET_ROOT = './snowyKITTI/dataset/sequences/'
+DATASET_ROOT = 'D:/snowyKITTI/dataset/sequences/'
+
 def snowRunLoop():
     configPath = './config/'
     # Load default config file
@@ -15,10 +18,10 @@ def snowRunLoop():
     # Update default config file with specific config file
     conf.__dict__.update(specificConf.__dict__)
 
-    for i in range(10):
+    for i in range(26):
         # Update the paths for the lidar and the labels
-        conf.lidarPath = './snowyKITTI/dataset/sequences/' + str(i).zfill(2) + '/snow_velodyne/'
-        conf.labelPath = './snowyKITTI/dataset/sequences/' + str(i).zfill(2) + '/snow_labels/'
+        conf.lidarPath = DATASET_ROOT + str(i).zfill(2) + '/snow_velodyne/'
+        conf.labelPath = DATASET_ROOT + str(i).zfill(2) + '/snow_labels/'
 
         # Update the simulation horizon
         files = [f for f in os.listdir(conf.lidarPath) if f.endswith('.bin')]
@@ -30,18 +33,19 @@ def snowRunLoop():
                 conf.isROR = True
                 conf.isSOR = False
                 conf.isDROR = False
+                logID = 'SnowyKitti-' + str(i).zfill(2) + '-' + filter + '-k-' + str(conf.ROR_k) + '-r-' + str(conf.ROR_r)
             elif filter == 'SOR':
                 conf.isROR = False
                 conf.isSOR = True
                 conf.isDROR = False
+                logID = 'SnowyKitti-' + str(i).zfill(2) + '-' + filter + '-k-' + str(conf.SOR_k) + '-s-' + str(conf.SOR_s)
             elif filter == 'DROR':
                 conf.isROR = False
                 conf.isSOR = False
                 conf.isDROR = True
+                logID = 'SnowyKitti-' + str(i).zfill(2) + '-' + filter + '-k-' + str(conf.DROR_k) + '-rho-' + str(conf.DROR_rho)
 
-            # Update the logID
             print('Running simulation ' + str(i) + ' with filter ' + filter)
-            logID = 'SnowyKitti-' + str(i).zfill(2) + '-' + filter
 
             # Run the simulation
             run(logID, conf)
