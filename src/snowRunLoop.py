54c80df046e2bb46b42b1151b0dbb9ca105fa6a8
unknown <jmgs@kth.se>
2025-01-08 12:07:00 +0100

    snowRunLoop: Add list of valid logs based on SLAM results

--- src/snowRunLoop.py
+++ src/snowRunLoop.py
@@ -5,6 +5,8 @@ import os
 #DATASET_ROOT = './snowyKITTI/dataset/sequences/'
 DATASET_ROOT = 'D:/snowyKITTI/dataset/sequences/'
 
+VALID_LOGS = [0, 2, 3, 5, 7, 8, 9, 11, 13, 14, 15, 16, 18, 19, 22, 23, 24, 25]
+
 def snowRunLoop():
     configPath = './config/'
     # Load default config file
@@ -18,10 +20,10 @@ def snowRunLoop():
     # Update default config file with specific config file
     conf.__dict__.update(specificConf.__dict__)
 
-    for i in range(26):
+    for log in VALID_LOGS:
         # Update the paths for the lidar and the labels
-        conf.lidarPath = DATASET_ROOT + str(i).zfill(2) + '/snow_velodyne/'
-        conf.labelPath = DATASET_ROOT + str(i).zfill(2) + '/snow_labels/'
+        conf.lidarPath = DATASET_ROOT + str(log).zfill(2) + '/snow_velodyne/'
+        conf.labelPath = DATASET_ROOT + str(log).zfill(2) + '/snow_labels/'
 
         # Update the simulation horizon
         files = [f for f in os.listdir(conf.lidarPath) if f.endswith('.bin')]
@@ -33,19 +35,19 @@ def snowRunLoop():
                 conf.isROR = True
                 conf.isSOR = False
                 conf.isDROR = False
-                logID = 'SnowyKitti-' + str(i).zfill(2) + '-' + filter + '-k-' + str(conf.ROR_k) + '-r-' + str(conf.ROR_r)
+                logID = 'SnowyKitti-' + str(log).zfill(2) + '-' + filter + '-k-' + str(conf.ROR_k) + '-r-' + str(conf.ROR_r)
             elif filter == 'SOR':
                 conf.isROR = False
                 conf.isSOR = True
                 conf.isDROR = False
-                logID = 'SnowyKitti-' + str(i).zfill(2) + '-' + filter + '-k-' + str(conf.SOR_k) + '-s-' + str(conf.SOR_s)
+                logID = 'SnowyKitti-' + str(log).zfill(2) + '-' + filter + '-k-' + str(conf.SOR_k) + '-s-' + str(conf.SOR_s)
             elif filter == 'DROR':
                 conf.isROR = False
                 conf.isSOR = False
                 conf.isDROR = True
-                logID = 'SnowyKitti-' + str(i).zfill(2) + '-' + filter + '-k-' + str(conf.DROR_k) + '-rho-' + str(conf.DROR_rho)
+                logID = 'SnowyKitti-' + str(log).zfill(2) + '-' + filter + '-k-' + str(conf.DROR_k) + '-rho-' + str(conf.DROR_rho)
 
-            print('Running simulation ' + str(i) + ' with filter ' + filter)
+            print('Running simulation ' + str(log) + ' with filter ' + filter)
 
             # Run the simulation
             run(logID, conf)
