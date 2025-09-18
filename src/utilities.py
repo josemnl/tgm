6dc9177eb3e90785cef6e95823713dd64922412d
unknown <jmgs@kth.se>
2025-09-18 10:27:30 +0200

    src/utilities.py: Fix error in readPose()

--- src/utilities.py
+++ src/utilities.py
@@ -10,7 +10,7 @@ from gridMap import pose, position, orientation
 def readPose(file):
     with open(file) as data:
         x_t = np.array([line.split(",") for line in data]).astype(float)[0]
-    return pose(position(x_t[0], x_t[1], 0.0), orientation(0.0, 0.0, x_t[5]))
+    return pose(position(x_t[0], x_t[1], 0.0), orientation(0.0, 0.0, x_t[2]))
 
 def read2DLidarCSV(file):
     with open(file) as data:
